ad233b43ca25a3923b6ae9c1561578b1aef02402
manjithd2 <manjith.dungdung@gmail.com>
2015-05-15 01:45:16 +0530

    modify app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from flask import *
 from flask_wtf import Form
+from flask.ext.sqlalchemy import SQLAlchemy
 from forms import VoteForm
 import sqlite3
 
 #config
-DATABASE = 'voting.db'
-SECRET_KEY = 'U\xdc\x06\xce\xd0\xd6\xa0\xd8P]\xe9\x1cD\xc8+bS\xc7Y\xca\xae\xfb\xd1rW'
-
-
 app = Flask(__name__)
- 
-app.config.from_object(__name__)
+app.secret_key=  'U\xdc\x06\xce\xd0\xd6\xa0\xd8P]\xe9\x1cD\xc8+bS\xc7Y\xca\xae\xfb\xd1rW'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///voting.db'
+
+
+db=SQLAlchemy(app)
+
+from models import *
 
 #connect to database
 def connect_db():
-  return sqlite3.connect(app.config['DATABASE'])
+  return sqlite3.connect('voting.db')
 
 @app.route("/")
 def home():
@@ -26,24 +28,25 @@ def vote():
   form = VoteForm()
  
   if request.method == 'POST':
-      g.db = connect_db()
       k= request.form.get('opt1')
       w= request.form.get('opt2')
       s= request.form.get('opt3')
       b= request.form.get('opt4')
       
       if k== 'opt1':
-        g.db.execute("INSERT INTO vote VALUES('opt1')")
+        k=VotePost('opt1')
+        db.session.add(k)
       if w=='opt2':
-        g.db.execute("INSERT INTO vote VALUES('opt2')")
+        w=VotePost('opt2')
+        db.session.add(w)
       if s=='opt3':
-        g.db.execute("INSERT INTO vote VALUES('opt3')")
+        s=VotePost('opt3')
+        db.session.add(s)
       if b=='opt4':
-        g.db.execute("INSERT INTO vote VALUES('opt4')")
-        
+        b=VotePost('opt4')
+        db.session.add(b)
      
-      g.db.commit()
-      g.db.close()
+      db.session.commit()
 
   elif request.method == 'GET':
     return render_template('vote.html', form=form)
@@ -55,7 +58,7 @@ def vote():
 def result():
   g.db= connect_db()
   cur=g.db.execute('select * from vote')
-  j = [dict(casts=row[0]) for row in cur.fetchall()]
+  j = [dict(casts=row[1]) for row in cur.fetchall()]
   g.db.close()
   op1=0
   op2=0
